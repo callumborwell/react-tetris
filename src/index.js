9146f10d96d66130739b5ec6b51fcdee467520f1
Callum Borwell <callum.borwell@gmail.com>
2021-11-30 21:12:01 +1100

    browserrouter check

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 //ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
+/* ReactDOM.render(
     <Router basename='/react-tetris'>
         <Routes>
             <Route exact path='/' component={App} />
         </Routes>
     </Router>, 
     document.getElementById('root')
+); */
+
+ReactDOM.render((
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+ ),
+ document.getElementById('root')
 );
