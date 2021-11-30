0bb18b300e2648dc931ade3c957c659ed3d5b421
Callum Borwell <callum.borwell@gmail.com>
2021-11-30 21:13:27 +1100

    brouwserrouter2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import BrowserRouter from 'react-router-dom';
 
 //ReactDOM.render(<App />, document.getElementById("root"));
 
